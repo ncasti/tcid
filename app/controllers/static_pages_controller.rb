30d1f8b9bbdb5ee729e7d1f9cf2aa3b8ac253755
Duolingo <Duolingo@Duolingos-MacBook-Pro-2.local>
2015-06-15 09:24:24 -0400

    question change, variety

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,19 +9,47 @@ class StaticPagesController < ApplicationController
   end
 
   def firstq
-      @question_1 = ActionController::Base.helpers.asset_path("spcuentanos.m4a")
+      @question_1 = ActionController::Base.helpers.asset_path("1-0.m4a")
   end
 
   def secondq
-      @question_2 = ActionController::Base.helpers.asset_path("spimagen.m4a")
+      @image_2 = ["2-0.png", "2-1.png", "2-3.jpg"]
+      
+      @question_2 = ActionController::Base.helpers.asset_path("2-0.m4a")
   end
 
   def thirdq
-      @question_3 = ActionController::Base.helpers.asset_path("spvacaciones.m4a")
+      audio_3 = [ "3-0.m4a", "3-1.m4a", "3-2.m4a"]
+      
+      @text_3 = [
+      
+      "Describe un restaurante que conoces. Debes decir:
+      1. Dónde queda este restaurante
+      2. Qué tipo de comida sirve el restaurante
+      3. Con qué frecuencia vas a este restaurante.
+      Explica por qué recomendarías este restaurante.",
+      
+      "Describe un país que te gustaría visitar. Debes decir:
+      1. El nombre del país
+      2. Cuándo te gustaría visitarlo
+      3. Qué harías en ese país.
+      Explica por qué te gustaría visitar ese país.",
+      
+      "Describe algo especial que quieres comprar en el futuro. Debes decir:
+      1. Qué objeto es
+      2. Dónde lo vas a comprar
+      3. Cuándo planeas comprarlo.
+      Explica por qué lo quieres comprar."]
+      
+      @randomnumber = rand(3)
+      @question_3 = ActionController::Base.helpers.asset_path(audio_3[@randomnumber])
+      
   end
 
   def extraq
-      @question_4 = ActionController::Base.helpers.asset_path("spvida.m4a")
+      @question_4 = ActionController::Base.helpers.asset_path("4-0.m4a")
+      
+
   end
   
   def feedback
